9efcb2d952c0490bb3a4e0cdc69573996486bca2
Chetan Sachdeva <chet.deva@gmail.com>
2022-05-04 19:36:22 +0530

    Init TerraFinderX. Add Home and Details screens

--- App.tsx
+++ App.tsx
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, Button, TextInput, View, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+function HomeScreen({ navigation }) {
+  const [searchText, onChangeSearchText] = React.useState('');
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <TextInput
+        style={styles.input}
+        placeholder="Search Block / Tx / Account"
+        onChangeText={onChangeSearchText}
+      />
+      <Button
+        title="Search"
+        onPress={() => navigation.navigate('Details', {
+          searchText: searchText
+        })}
+      />
     </View>
   );
 }
 
+function DetailsScreen({ route, navigation }) {
+  const { searchText } = route.params;
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Searched for {JSON.stringify(searchText)}</Text>
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="TerraFinderX" component={HomeScreen} />
+      <Stack.Screen name="Details" component={DetailsScreen} />
+    </Stack.Navigator>
+  );
+}
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
